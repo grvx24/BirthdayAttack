3040641fe34e37b7f7d93e5b9467c1b91ad693ba
grvx24 <kamil.sagalara@student.put.poznan.pl>
2018-12-04 17:28:13 +0100

    generating random 4 bytes messages

--- DataGenerator.cs
+++ DataGenerator.cs
@@ -9,6 +9,11 @@ namespace BirthdayAttack
 {
     class DataGenerator : IDataGenerator
     {
+        public delegate void UpdateStep(int counter,int max);
+        public delegate void CompleteStep();
+        public event UpdateStep UpdateEvent;
+        public event CompleteStep CompleteEvent;
+
         public int[] GenerateUniqueIntegers(int length)
         {
             if (length > Int32.MaxValue / 2)
@@ -19,13 +24,26 @@ namespace BirthdayAttack
             Random rng = new Random();
 
             int startPoint = rng.Next(int.MinValue, int.MaxValue-length);
+            int endPoint = startPoint + length;
+            int difference = endPoint - startPoint;
             int[] result = new int[length];
             int counter = 0;
-            for (int i = startPoint; i < startPoint+length; i++)
+
+            for (int i = startPoint; i < endPoint; i++)
             {
                 result[counter] = i;
                 counter++;
+
+                //update every 1000 steps
+                if (counter % 1000 == 0)
+                {
+                    if (UpdateEvent != null)
+                        UpdateEvent.Invoke(counter, difference);
+                }
             }
+
+            if(CompleteEvent!=null)
+                CompleteEvent.Invoke();
 
             return result;
 
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace BirthdayAttack
 {
@@ -22,6 +24,8 @@ namespace BirthdayAttack
     /// </summary>
     public partial class MainWindow : Window
     {
+        private byte[] loadedData;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -34,23 +38,90 @@ namespace BirthdayAttack
 
         private void LoadMessages_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == true)
+            {
+                loadedData = File.ReadAllBytes(fileDialog.FileName);
+            }
+
+            if (loadedData != null)
+            {
+                fileName_label.Content = "File name: "+fileDialog.SafeFileName;
+                fileSize_label.Content = "File size: " + loadedData.Length + " bytes";
+                MessagesNumber_label.Content = "Number of messages: " + loadedData.Length / sizeof(int);
+            }
+            else
+            {
+                fileName_label.Content = "File name: ";
+                fileSize_label.Content = "File size: 0";
+                MessagesNumber_label.Content = "Number of messages: 0";
+            }
 
         }
 
         private void GenerateHashes_Click(object sender, RoutedEventArgs e)
         {
-
+            if (loadedData == null)
+            {
+                MessageBox.Show("No file loaded!");
+                return;
+            }
         }
 
         private void GenerateMessage_Click(object sender, RoutedEventArgs e)
         {
-            DataGenerator dg = new DataGenerator();
-            var result=dg.GenerateUniqueIntegers(50);
-
-            foreach (var n in result)
-            {
-                Trace.WriteLine(n);
+            Regex regex = new Regex("[^0-9]+");
+            string numOfMessages = message_num.Text;
+            if (regex.IsMatch(numOfMessages))
+            {   
+                MessageBox.Show("Invalid length parameter!");
+                return;
             }
+
+            DataGenerator dg = new DataGenerator();
+            //events
+            dg.UpdateEvent += (int counter, int max) =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    GeneratingData_label.Visibility = Visibility.Visible;
+                    var percents = ((counter * 100) / max);
+                    Generation_loading.Value = percents;
+                    Loading_percents.Content = percents + "%";
+                });
+
+
+            };
+            dg.CompleteEvent += () =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    GeneratingData_label.Visibility = Visibility.Hidden;
+                    Loading_percents.Content = "100%";
+                });
+            };
+
+            Task.Run(() =>
+            {
+                var result = dg.GenerateUniqueIntegers(int.Parse(numOfMessages));
+
+                Dispatcher.Invoke(() =>
+                {
+                    SaveFileDialog fileDialog = new SaveFileDialog();
+                    if (fileDialog.ShowDialog() == true)
+                    {
+                        var stream = File.Open(fileDialog.FileName, FileMode.Create);
+                        using (BinaryWriter bw = new BinaryWriter(stream))
+                        {
+                            for (int i = 0; i < result.Length; i++)
+                            {
+                                bw.Write(result[i]);
+                            }                          
+                        }
+                    }
+                });
+
+            });
         }
 
         private void LoadHashes_Click(object sender, RoutedEventArgs e)
